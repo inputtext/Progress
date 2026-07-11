e146225bcaa0f8ae88e7253143ddaf52754f257a
Piyush Kanojiya <raat131221@gmail.com>
2026-07-11 21:12:27 +0530

    11/7/26

--- React-vite/forpractise/src/App.jsx
+++ React-vite/forpractise/src/App.jsx
@@ -1,12 +1,27 @@
-import About from "./About";
-let App =()=>{
+import React ,{useState} from "react";
+
+const App = ()=>{
+    let [count,setCount] =useState(0);
+
+
+
     return (
-    <About width="100px"/>
+        <div>
+            <h1>count is : {count}</h1>
+            <button onClick={()=>{
+                setCount(count+=1);
+            }}>INCREMENT</button>
+
+            <button onClick={()=>{
+                setCount(count-count);
+            }}>RESET</button>
+
+            <button onClick={()=>{
+                setCount(count+=10);
+            }}>increment by 10</button>
+        </div>
     )
-};
+}
+
 
 export default App;
-
-
-
-
